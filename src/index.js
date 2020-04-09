cb44ba1e1d5ed3c415c23c03394d475e819c3bd9
sumit jolly <sumit.jolly@finleap.com>
2020-04-09 22:01:08 +0200

    include gitignore

--- src/index.js
+++ src/index.js
@@ -2,25 +2,23 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const User = require("./user");
+const User = require("./model/user");
+const cors = require("cors");
 
+app.use(cors());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
 app.use(bodyParser.json());
-// Connect to Mongoose and set connection constiable
-mongoose.connect("mongodb://localhost:27017/LAB", { useNewUrlParser: true });
+
+mongoose.connect(process.env.DB_HOST || "mongodb://localhost:27017/LAB", { useNewUrlParser: true });
 const db = mongoose.connection;
 
-// Added check for DB connection
 if (!db) console.log("Error connecting db");
 else console.log("Db connected successfully");
 
-const port = process.env.PORT || 8080;
-
-// Send message for default URL
 app.get("/users", async (req, res) => {
   const result = await User.find().exec();
   console.log(result);
@@ -29,18 +27,14 @@ app.get("/users", async (req, res) => {
 
 app.post("/user", async (req, res) => {
   const user = new User(req.body);
-  console.log(user);
-  await user
-    .save()
-    .then((doc) => {
-      console.log(doc);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  await user.save().catch((err) => {
+    console.error(err);
+  });
   res.send("User is created.");
 });
-// Launch app to listen to specified port
+
+const port = process.env.PORT || 8080;
+
 app.listen(port, function () {
   console.log("Running Node server on port " + port);
 });
